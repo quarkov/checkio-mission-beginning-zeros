0e27961926ee6fece4568cc92114a58990928c99
Alexander Lyabah <a.lyabah@checkio.org>
2020-03-02 13:05:34 +0200

    first version

--- autofill.sh
+++ autofill.sh
@@ -1,1 +1,1 @@
-checkio --domain py autofillrepo --py-function sum_two --js-function sumTwo .
+checkio --domain py autofillrepo --py-function beginning_zeros --js-function beginningZeros --desc-tests 6 .
--- verification/tests.py
+++ verification/tests.py
@@ -11,26 +11,50 @@ Each test is a dict with
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": ['100'],
+            "answer": 0,
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+            "input": ['001'],
+            "answer": 2,
+        },
+        {
+            "input": ['100100'],
+            "answer": 0,
+        },
+        {
+            "input": ['001001'],
+            "answer": 2,
+        },
+        {
+            "input": ['012345679'],
+            "answer": 1,
+        },
+        {
+            "input": ['0000'],
+            "answer": 4,
+        },
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": ['734'],
+            "answer": 0,
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+            "input": ['110'],
+            "answer": 0,
+        },
+        {
+            "input": ['567'],
+            "answer": 0,
+        },
+        {
+            "input": ['0'],
+            "answer": 1,
+        },
+        {
+            "input": ['1'],
+            "answer": 0,
+        },
     ]
 }
